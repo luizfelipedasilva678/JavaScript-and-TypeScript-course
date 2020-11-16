c9aa4826570551a1c172bc65a0166b4e07933ee1
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-11-16 20:36:10 -0300

    Aula sobre 'For'

---
+++ aula24/main.js
@@ -0,0 +1,15 @@
+for(let i = 1; i<11; i++) {
+    if(i%2 === 0) {
+        console.log('Par');
+    }else {
+        console.log('Ímpar');
+    }
+}
+
+
+let j = 0;
+while(j  < 8) {
+    console.log('Linha com while ' + j);
+
+    j++;
+}
