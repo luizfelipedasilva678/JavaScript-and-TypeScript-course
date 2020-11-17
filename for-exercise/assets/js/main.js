dcf4989858f2d69a82ee8d16253b3f3476592dba
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-11-16 21:09:00 -0300

    Changing file

--- for-exercise/assets/js/main.js
+++ for-exercise/assets/js/main.js
@@ -1,6 +1,4 @@
 (function () {
-    let container = document.querySelector('.container');
-
     const elementos = [
         {tag: 'section', texto: 'Frase 1'},
         {tag: 'div', texto: 'Frase 2'},
@@ -8,13 +6,13 @@
         {tag: 'section', texto: 'Frase 4'},
     ]
 
-
     for(let i=0; i<elementos.length; i++) {
         const {tag, texto} = elementos[i];
         createElement(tag, texto);
     }
 
     function createElement(element, value) {
+        let container = document.querySelector('.container');
         let DOMelement = document.createElement(element);
         DOMelement.innerHTML= value;
         container.appendChild(DOMelement);
