bd4b92c737d13997d221575920fbce0bc3fcd241
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-11-17 20:55:29 -0300

    While and Do while

---
+++ while-dowhile/main.js
@@ -0,0 +1,14 @@
+let continuar = confirm("Deseja continuar ?");
+
+/*
+do {
+    alert('Olá');
+
+    continuar = confirm("Deseja continuar ?");
+}while(continuar)
+*/
+
+while(continuar) {
+    alert('Olá');
+    continuar = confirm("Quer continuar ?")
+}
