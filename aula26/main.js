6e3ea67ae91f70dafbaab4edfbd9eed91cdd4587
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-11-20 22:05:13 -0300

    aula de funcoes

---
+++ aula26/main.js
@@ -0,0 +1,15 @@
+oi();
+executaFucao(souUmDado);
+
+function oi() {
+    console.log('oi');
+}
+
+function souUmDado() {
+    console.log('sou-um-dado');
+}
+
+function executaFucao(funcao) {
+    funcao();
+}
+
