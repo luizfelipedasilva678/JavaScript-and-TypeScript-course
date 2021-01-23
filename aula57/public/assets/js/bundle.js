1aa2c554fbfabf27f8af3934cc2c6b5011ccef36
Luiz <luizfelipedasilva678@gmail.com>
2021-01-22 21:37:50 -0300

    Webpack Boilerplate

---
+++ aula57/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var ola = 'ola';
+console.log(ola);
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ aula57/src/index.js
@@ -0,0 +1,3 @@
+const ola = 'ola';
+
+console.log(ola);
