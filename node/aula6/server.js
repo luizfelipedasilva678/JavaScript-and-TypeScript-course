abca593a4d1340804513b419b330bbcea8405f2f
Luiz <luizfelipedasilva678@gmail.com>
2021-02-01 20:22:02 -0300

    Express

---
+++ node/aula6/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato com a gente');
+});
+
+app.listen(3000);
