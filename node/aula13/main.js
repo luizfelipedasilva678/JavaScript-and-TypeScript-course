dfeeec6bb9fb9fb61c58bdf0ba7de84688b114e3
Luiz <luizfelipedasilva678@gmail.com>
2021-02-03 21:05:37 -0300

    Middlewares

---
+++ node/aula13/main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+const middleware = require('./src/middleware/middleware');
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(middleware, routes);
+
+
+app.listen(3000); 
